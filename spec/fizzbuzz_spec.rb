26f7339f757f2fc13854e37a1c3ec0568f3989d5
Noora <n.k.alqasimi@gmail.com>
2017-02-06 15:16:05 +0000

    added % 15 test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "Fizz" when passed 3' do
+  it 'returns "Fizz" when passed a multiple of 3' do
     expect(fizzbuzz(3)).to eq 'Fizz'
     expect(fizzbuzz(6)).to eq 'Fizz'
     expect(fizzbuzz(12)).to eq 'Fizz'
@@ -9,14 +9,20 @@ describe 'fizzbuzz' do
   end
 
 describe 'fizzbuzz' do
-  it 'returns "Buzz" when passed 5' do
+  it 'returns "Buzz" when passed multiple of 5' do
     expect(fizzbuzz(5)).to eq 'Buzz'
     expect(fizzbuzz(20)).to eq 'Buzz'
     expect(fizzbuzz(50)).to eq 'Buzz'
     expect(fizzbuzz(100)).to eq 'Buzz'
   end
 
-
+describe 'fizzbuzz'
+  it 'returns "Fizzbuzz" when passed multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'Fizzbuzz'
+    expect(fizzbuzz(45)).to eq 'Fizzbuzz'
+    expect(fizzbuzz(75)).to eq 'Fizzbuzz'
+    expect(fizzbuzz(150)).to eq 'Fizzbuzz'
+  end
 
 end
 
