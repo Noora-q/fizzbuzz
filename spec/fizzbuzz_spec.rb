6a06c428071f263c4950984aecf4d41ba6d4a8ba
Noora <n.k.alqasimi@gmail.com>
2017-02-06 14:38:07 +0000

    added % 3 test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,5 +3,8 @@ require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
+    expect(fizzbuzz(12)).to eq 'fizz'
+    expect(fizzbuzz(99)).to eq 'fizz'
   end
 end
