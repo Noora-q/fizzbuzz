09ac9e76fcc1b694510d340ce1b934af413910f3
Bernard Malhame <bernard.malhame@gmail.comgit config --global core.autocrlf inputgit config --global user.email bernard.malhame@gmail.com>
2017-02-06 15:08:36 +0000

    added % 5 test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,23 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-    expect(fizzbuzz(6)).to eq 'fizz'
-    expect(fizzbuzz(12)).to eq 'fizz'
-    expect(fizzbuzz(99)).to eq 'fizz'
+  it 'returns "Fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+    expect(fizzbuzz(6)).to eq 'Fizz'
+    expect(fizzbuzz(12)).to eq 'Fizz'
+    expect(fizzbuzz(99)).to eq 'Fizz'
   end
+
+describe 'fizzbuzz' do
+  it 'returns "Buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+    expect(fizzbuzz(20)).to eq 'Buzz'
+    expect(fizzbuzz(50)).to eq 'Buzz'
+    expect(fizzbuzz(100)).to eq 'Buzz'
+  end
+
+
+
+end
+
 end
